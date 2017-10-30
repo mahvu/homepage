35215cb88465951324e8ebd8ff5b0c35e7ce4ffe
Lauri Rasi <rasil@student.tut.fi>
2017-10-30 10:08:13 +0200

    remade as a single-page app

--- index.js
+++ index.js
@@ -2,19 +2,11 @@ import React from 'react'
 import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 import { render } from 'react-dom'
 import App from './modules/App'
-import Home from './modules/home'
-import Projects from './modules/projects'
-import Project from './modules/project'
-import CV from './modules/cv'
+
 
 render((
-    <Router history={browserHistory}>
-        <Route path="/" component={App}>
-        <IndexRoute component={Home}/>
-            <Route path="/projects" component={Projects}>
-                <Route path="/projects/:projectName" component={Project}/>
-            </Route>
-            <Route path="/cv" component={CV}/>
-        </Route>
-    </Router>
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+    </Route>
+  </Router>
   ), document.getElementById('app'))
--- modules/App.js
+++ modules/App.js
@@ -1,33 +1,77 @@
 import React from 'react'
-//import { IndexLink } from 'react-router'
-import NavLink from './NavLink'
 
 export default React.createClass({
   render() {
     return (
 
       <div>
-      
-        <div className="topBar">
-          <ul className="extLinks">
-            <li><a href="https://www.linkedin.com/in/lrasi/" target="_blank"> <img src="/linkedin.png" alt="toMyLinkedinProfile"/> </a></li>
-            <li><a href="https://github.com/mahvu" target="_blank"> <img src="/github.png" alt="toMyGitHubProfile"/> </a></li>
-          </ul>
+
+        <div className="header" id="header">
+          <h1> Lauri Rasi </h1>
         </div>
 
+        <div className="container">
 
-          <ul role="nav" className="header">
-            <li><NavLink to="/" onlyActiveOnIndex={true}>Home</NavLink></li>
-            <li><NavLink to="/cv">CV</NavLink></li>
-            <li><NavLink to="/projects">Projects</NavLink></li>
-          </ul>
+          <div className="row">
+
+            <div className="one-half column">
+
+              <h2>
+              <i className="fa fa-male"></i>
+              &nbsp;About me 
+              </h2>
+
+              <p>My name is Lauri Rasi. I'm a bachelor of science, currently working on my master's degree in software engineering at 
+              &nbsp;<a href="http://www.tut.fi/en/home">Tampere University of Technology.</a>&nbsp; 
+              I am also an esports enthusiast and a retired esports professional, atleast for now. Currently I am pursuing a career in software industry.
+              <br />
+              <br />
+              My interests lie in various web technologies, especially in JavaScript. However I also love to get my hands dirty with little Arduino projects from time to time.
+              </p>
+
+            </div>
+
+            <div className="one-half column">
+
+              <h2>
+              <i className="fa fa-graduation-cap"></i>
+              &nbsp;Reference 
+              </h2>
+
+              <p>Best option for browsing the projects I've been involved in is to check out my <a href="https://github.com/mahvu">Github profile</a>&nbsp;
+              I have also prepared short descriptions of my personal projects on <a href="">this page</a>. 
+              <br />
+              <br />
+
+              </p>
+
+            </div>
+          </div>
+
+          <h2>
+          <i className="fa fa-file-text"></i>
+          &nbsp;CV
+          </h2>
+
+          <p>You can view my CV <a href="/CV_Rasi.pdf">here</a>. Currently it is only available in finnish.
+          </p>
 
 
-        <div className="content">
-          {this.props.children}
-        </div>
+        </div>  
         
+    <div className="footer">
+      Lauri Rasi
+
+      <div itemscope itemtype="http://schema.org/Person">
+        <link itemprop="url" href="laurira.si"/>
+        <a itemprop="sameAs" href="https://twitter.com/mafu_ow"> <i className="fa fa-twitter"></i> </a>
+        <a itemprop="sameAs" href="https://github.com/mahvu"> <i className="fa fa-git"></i> </a>
+        <a itemprop="sameAs" href="https://www.linkedin.com/in/lrasi/"> <i className="fa fa-linkedin"></i> </a>
       </div>
+    </div>
+
+    </div>
+      
     )
   }
 })
